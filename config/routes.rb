67c8067ace282e9c2a66388e97d6f3f3e2433205
kse201 <kse.201@gmail.com>
2018-04-02 21:11:06 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+
   get 'static_pages/help'
 
   get 'static_pages/about'
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,5 +12,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get contact_path
 
     assert_select 'title', full_title('Contact')
+
+    get signup_path
+    assert_template 'users/new'
   end
 end
