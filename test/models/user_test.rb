6128432ae65e7c876a0dd4add59fb850e96db8a5
kse201 <kse.201@gmail.com>
2018-11-07 23:30:25 +0900

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,7 +8,7 @@ class UserTest < ActiveSupport::TestCase
     )
   end
 
-  test 'should)be valid' do
+  test 'should be valid' do
     assert @user.valid?
   end
 
@@ -82,5 +82,9 @@ class UserTest < ActiveSupport::TestCase
   test 'password should have a minimum length' do
     @user.password = @user.password_confirmation = 'a' * 5
     assert_not @user.valid?
+  end
+
+  test 'authenticated? should return false for a user with nil diget' do
+    assert_not @user.authenticated?('')
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,20 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # テストユーザとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: {
+      session: {
+        email: user.email,
+        password: password,
+        remember_me: remember_me
+      }
+    }
+  end
 end
